a394a16238ae291a6a0067ccbf8c49d187085c6c
Aaditya Uppal <aadityauppal@gmail.com>
2014-12-19 19:48:50 +0530

    submitting programming assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a matrix object that can cache its inverse
+## cacheSolve computes the inverse of the matrix returned by makeCacheMatrix
+## If the inverse has already been calculated then the cacheSolve retrieves
+## the inverse from the cache.
 
-## Write a short comment describing this function
+## Takes values and size of a matrix to return a special
+## "matrix" object that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(a,b){    
+  x<-matrix(a,b,b)                 ##Defines x as a square matrix
+  n<-NULL                          ##Sets initial value of n as NULL 
+  set<-function(y){                ##In 2nd environment assigns 
+    x<<-y                          ##matrix x to set
+    n<<-NULL                       ##default value of n as NULL
+  }
+  get<-function() x                ##gets value of x from 1st environment
+  setinverse<-function(solve) n<<-solve ##calculates and sets inverse 
+  ##as m in 2nd environment
+  getinverse<-function() n            ##gets inverse from n
+  list(set=set,get=get,            ##creates list vector with 4 variables
+       setinverse=setinverse,getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the matrix returned by makeCacheMatrix
+## If the inverse has already been calculated then the cacheSolve retrieves
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  n <- x$getinverse()               ##Gets value of n from getinverse
+  if(!is.null(n)) {                 
+    message("getting cached data")
+    return(n)
+  }
+  data <- x$get()                   ##If NULL, gets matrix and solves for inverse
+  n <- solve(data, ...)
+  x$setinverse(n)                   ##Inverse fed into setinverse 
+  n                                 ## Returns a matrix that is the inverse of 'x'
 }
